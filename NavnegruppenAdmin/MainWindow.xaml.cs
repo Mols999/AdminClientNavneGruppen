26220d7f2f2899422dcf9d90767677948e092e74
Mols999 <t.f.m@live.dk>
2023-12-06 14:20:07 +0100

    Initial commit

--- NavnegruppenAdmin/MainWindow.xaml.cs
+++ NavnegruppenAdmin/MainWindow.xaml.cs
@@ -1,12 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -15,9 +9,6 @@ using System.Windows.Shapes;
 
 namespace NavnegruppenAdmin
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
